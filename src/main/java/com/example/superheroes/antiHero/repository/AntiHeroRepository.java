6718389073e281f58b93ffcdc539c4309cc3d1e7
Fuat_Safiulin <fuatsafiulin@gmail.com>
2023-10-12 15:59:58 +0100

    GitHub actions

--- src/main/java/com/example/superheroes/antiHero/repository/AntiHeroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.superheroes.antiHero.repository;
-
-import com.example.superheroes.antiHero.entity.AntiHeroEntity;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface AntiHeroRepository extends CrudRepository<AntiHeroEntity, UUID> {
-}
